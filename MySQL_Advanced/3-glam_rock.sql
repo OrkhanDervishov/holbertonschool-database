f7f6bc4b230f229d8beb2f80de9044b4cd36389a
OrkhanDervishov <derviorxan7@gmail.com>
2026-03-16 15:18:46 -0400

    tasks

--- MySQL_Advanced/3-glam_rock.sql
+++
@@ -1,6 +0,0 @@
--- List all Glam rock bands ranked by their lifespan (in years)
-
-SELECT band_name, (split - formed) AS lifespan
-FROM metal_bands
-WHERE main_style = 'Glam rock'
-ORDER BY lifespan DESC;
--- MySQL_Advanced/5-valid_email.sql
+++
@@ -1,9 +0,0 @@
--- Reset valid_email only when the email is changed
--- If the email remains the same, valid_email should not change
-
-CREATE TRIGGER reset_valid_email
-BEFORE UPDATE ON users
-FOR EACH ROW
-IF OLD.email <> NEW.email THEN
-    SET NEW.valid_email = 0;
-END IF;
