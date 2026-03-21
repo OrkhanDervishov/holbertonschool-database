6e0904f8ed6ba9f69ef00f8d50444942aa9f9c0b
OrkhanDervishov <derviorxan7@gmail.com>
2026-03-21 17:26:24 +0400

    redis-task

--- redis_basic/exercise.py
+++ redis_basic/exercise.py
@@ -1,32 +1,62 @@
 #!/usr/bin/env python3
 """
-Doc
+This module has a Cache class with __init__ method and a private variable 
 """
-
 import uuid
-from typing import Union
-
-try:
-    import redis
-except ImportError:
-    redis = None
+from typing import Union, Callable, Optional
+from functools import wraps
 
 
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+                
 class Cache:
-    
-    """Doc"""
+    """
+    A class with __init__ method and a private variable
+    """
+    def __init__(self) -> None:
+        """
+        Method that initializes redis instance and flushes the db
+        """
+        import redis
+        
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
-    def __init__(self):
-        """Doc"""
-        if redis:
-            self._redis = redis.Redis()
-            self._redis.flushdb()
-        else:
-            self._redis = None
-
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Doc"""
+        """
+        Metod that takes uuid data type and returns str
+        """
         key = str(uuid.uuid4())
-        if self._redis:
-            self._redis.set(key, data)
+        self._redis.set(key, data)
+
+        return key
+    
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[int, bytes, float, str, None]:
+        """
+        Retrieves data from redis and applies optional conversion
+        """
+        data = self._redis.get(key)
+        if key is not None and fn is not None:
+            return fn(data) 
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Converts redis bytes into string
+        """
+        key = self.get(key, lambda d: d.decode("utf-8"))
+        return key
+
+    def get_int(self, key: int) -> int:
+        """
+        Converts redis bytes into integer
+        """
+        key = self.get(key, int)
         return key
